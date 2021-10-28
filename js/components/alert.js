7e952939f673ad39b824885735fc78907c7fc361
Farshad Sidiqi <farshad99@users.noreply.github.com>
2021-10-28 10:58:12 +0200

    done

--- js/components/alert.js
+++
@@ -1,9 +0,0 @@
-export default function alert(cssClass, errorMessage) {
-	document.querySelector('.alert').innerHTML = `<div class="alert  ${cssClass}">
-    ${errorMessage}
-  </div>`;
-
-	setTimeout(() => {
-		document.querySelector('.alert').innerHTML = '';
-	}, 3000);
-}
